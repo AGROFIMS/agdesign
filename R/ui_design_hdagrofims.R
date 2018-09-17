d1724d13aecb4f5c0737e53d172c7701a532483f
CIP-RIU <cip-riu@cgiar.org>
2018-09-17 16:09:41 +0000

    activate session buttons in develop version

--- R/ui_design_hdagrofims.R
+++ R/ui_design_hdagrofims.R
@@ -231,24 +231,24 @@ ui_fieldbook_agrofims <- function(type="tab",title="Design Fieldbook",name="phen
   shinydashboard::tabItem(
     tabName = name,
 
-    # fluidRow(
-    #   column(6, style = "margin-top: -16px; margin-bottom: 16px;", h1("Experiment description")),
-    #   column(6, align = "right", style = "margin-top: 11px;",
-    #          useShinyalert(),
-    #          #actionButton("xtest", "Test"),
-    #          actionButton('newfieldbook', 'New', icon("file"), class = "btn-primary", style="color: #fff;", width = "75px"),
-    #          actionButton('openfieldbook', 'Open', icon("folder-open"), width = "75px", onclick = "openTab('openFieldbook')"),
-    #          actionButton('savefieldbook', 'Save', icon("save"), class = "btn-success", style="color: #fff;", width = "75px"),
-    #          htmlOutput("lastsaved")
-    #          #uiOutput("saveUI")
-    #   )
-    # ),
+    fluidRow(
+      column(6, style = "margin-top: -16px; margin-bottom: 16px;", h1("Experiment description")),
+      column(6, align = "right", style = "margin-top: 11px;",
+             useShinyalert(),
+             #actionButton("xtest", "Test"),
+             actionButton('newfieldbook', 'New', icon("file"), class = "btn-primary", style="color: #fff;", width = "75px"),
+             actionButton('openfieldbook', 'Open', icon("folder-open"), width = "75px", onclick = "openTab('openFieldbook')"),
+             actionButton('savefieldbook', 'Save', icon("save"), class = "btn-success", style="color: #fff;", width = "75px"),
+             htmlOutput("lastsaved")
+             #uiOutput("saveUI")
+      )
+    ),
 
     fluidRow(
       column(12, verbatimTextOutput("text1"))
     ),
 
-    h1("Experiment description"),
+    #h1("Experiment description"),
 
     # To reset panels and UI
     shinyjs::useShinyjs(),
