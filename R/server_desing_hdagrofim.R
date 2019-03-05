10dd44795c371bd2e39beae57de4ce04cf1e130f
ivanperezm <ivanpe834@gmail.com>
2019-03-05 21:33:21 +0000

    up

--- R/server_desing_hdagrofim.R
+++ R/server_desing_hdagrofim.R
@@ -8552,36 +8552,34 @@ server_design_agrofims <- function(input, output, session, values){
            "crop_measurement_Sweetpotato", "crop_measurement_Wheat", "crop_measurement_Other")
   
   
-  observe({
-    input$fbDesignNav
-    isolate({
-      ct <- map_singleform_values(input$croppingType, type = "combo box", format = "vector", default = "Monocrop")
-      #print(ct)
-      if (ct == "Intercrop") {
-        id_ic_rand <- getAddInputId(intercropVars$ids, "IC_", "")
-        #print(id_ic_rand)
-        circm <- map_values(input, id_chr="cropCommonNameInter_", id_ic_rand, format = "vector", lbl= "Select crop")
-        #print(circm)
-        cropivan <- paste0("crop_measurement_", circm)
-      } else{
-        #if(ct=="Monocrop"){
-        crp <- map_singleform_values(input$cropCommonNameMono, input_other = input$cropCommonNameMono_other, type= "combo box", format = "vector", label = "Crop",default = "Maize")
-        #print(crp)
-        cropivan <- paste0("crop_measurement_",crp)
-        #var<- map_singleform_values(input$cultivarNameMono, type= "combo box", format = "data.frame",label = "Crop variety(s)",collapsed = TRUE)
-        #out <- rbind(ctd, crp, var)
-        #}
-      }
+  observeEvent(input$fbDesignNav, {
       
-      for (i in 1:length(chu)) {
-        shiny::hideTab(inputId = "tabpanelinter", target = chu[i])
-      }
-  
-      for (i in 1:length(cropivan)) {
-        #print(gsub(" ","",cropivan[i]))
-        shiny::showTab(inputId = "tabpanelinter", target = gsub(" ","",cropivan[i]), select = T)
-      }
-    })
+    ct <- map_singleform_values(input$croppingType, type = "combo box", format = "vector", default = "Monocrop")
+    #print(ct)
+    if (ct == "Intercrop") {
+      id_ic_rand <- getAddInputId(intercropVars$ids, "IC_", "")
+      #print(id_ic_rand)
+      circm <- map_values(input, id_chr="cropCommonNameInter_", id_ic_rand, format = "vector", lbl= "Select crop")
+      #print(circm)
+      cropivan <- paste0("crop_measurement_", circm)
+    } else{
+      #if(ct=="Monocrop"){
+      crp <- map_singleform_values(input$cropCommonNameMono, input_other = input$cropCommonNameMono_other, type= "combo box", format = "vector", label = "Crop",default = "Maize")
+      #print(crp)
+      cropivan <- paste0("crop_measurement_",crp)
+      #var<- map_singleform_values(input$cultivarNameMono, type= "combo box", format = "data.frame",label = "Crop variety(s)",collapsed = TRUE)
+      #out <- rbind(ctd, crp, var)
+      #}
+    }
+    
+    for (i in 1:length(chu)) {
+      shiny::hideTab(inputId = "tabpanelinter", target = chu[i])
+    }
+    
+    for (i in 1:length(cropivan)) {
+      #print(gsub(" ","",cropivan[i]))
+      shiny::showTab(inputId = "tabpanelinter", target = gsub(" ","",cropivan[i]), select = T)
+    }
     
   })
   
@@ -8945,34 +8943,33 @@ server_design_agrofims <- function(input, output, session, values){
   chuphe <- c("crop_phenology_Cassava", "crop_phenology_Commonbean", "crop_phenology_Maize", "crop_phenology_Potato", "crop_phenology_Rice",
            "crop_phenology_Sweetpotato", "crop_phenology_Wheat", "crop_phenology_Other")
   
-  observe({
-    input$fbDesignNav
-    isolate({
-      ct <- map_singleform_values(input$croppingType, type = "combo box", format = "vector", default = "Monocrop")
-
-      if (ct == "Intercrop") {
-        id_ic_rand <- getAddInputId(intercropVars$ids, "IC_", "")
-        circm <- map_values(input, id_chr="cropCommonNameInter_", id_ic_rand, format = "vector", lbl= "Select crop")
-        #print(circm)
-        cropivanphe <- paste0("crop_phenology_", circm)
-      } else{
-        #if(ct=="Monocrop"){
-        crp <- map_singleform_values(input$cropCommonNameMono,input_other = input$cropCommonNameMono_other, type= "combo box", format = "vector", label = "Crop",default = "Maize")
-        cropivanphe <- paste0("crop_phenology_",crp)
-        #var<- map_singleform_values(input$cultivarNameMono, type= "combo box", format = "data.frame",label = "Crop variety(s)",collapsed = TRUE)
-        #out <- rbind(ctd, crp, var)
-        #}
-      }
-
-      for (i in 1:length(chuphe)) {
-        shiny::hideTab(inputId = "tabpanelinterphe", target = chuphe[i])
-      }
-
-      for (i in 1:length(cropivanphe)) {
-        #print(gsub(" ","",cropivanphe[i]))
-        shiny::showTab(inputId = "tabpanelinterphe", target = gsub(" ","",cropivanphe[i]), select = T)
-      }
-    })
+  observeEvent(input$fbDesignNav, {
+      
+    ct <- map_singleform_values(input$croppingType, type = "combo box", format = "vector", default = "Monocrop")
+    
+    if (ct == "Intercrop") {
+      id_ic_rand <- getAddInputId(intercropVars$ids, "IC_", "")
+      circm <- map_values(input, id_chr="cropCommonNameInter_", id_ic_rand, format = "vector", lbl= "Select crop")
+      #print(circm)
+      cropivanphe <- paste0("crop_phenology_", circm)
+    } else{
+      #if(ct=="Monocrop"){
+      crp <- map_singleform_values(input$cropCommonNameMono,input_other = input$cropCommonNameMono_other, type= "combo box", format = "vector", label = "Crop",default = "Maize")
+      cropivanphe <- paste0("crop_phenology_",crp)
+      #var<- map_singleform_values(input$cultivarNameMono, type= "combo box", format = "data.frame",label = "Crop variety(s)",collapsed = TRUE)
+      #out <- rbind(ctd, crp, var)
+      #}
+    }
+    
+    for (i in 1:length(chuphe)) {
+      shiny::hideTab(inputId = "tabpanelinterphe", target = chuphe[i])
+    }
+    
+    for (i in 1:length(cropivanphe)) {
+      #print(gsub(" ","",cropivanphe[i]))
+      shiny::showTab(inputId = "tabpanelinterphe", target = gsub(" ","",cropivanphe[i]), select = T)
+    }
+    
   })
   #### End Tabs Crop Phenology: ####
   
